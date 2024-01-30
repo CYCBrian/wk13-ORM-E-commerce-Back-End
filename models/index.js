0ce712305c290a07fd4790bd3478fb32a08f2fa2
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-01-29 21:39:10 -0500

    7.Testing - Fixed create a product...kind of

--- models/index.js
+++ models/index.js
@@ -40,6 +40,28 @@ Tag.belongsToMany(Product,{
   onUpdate:'CASCADE'
 })
 
+// // Products belongsTo Category
+// Product.belongsTo(Category, {
+//   foreignKey: 'category_id'
+// });
+
+// // Categories have many Products
+// Category.hasMany(Product, {
+//   foreignKey: 'product_id'
+// });
+
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//   through: ProductTag,
+//   foreignKey: "product_id"
+// });
+
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   through: ProductTag,
+//   foreignKey: "tag_id"
+// });
+
 module.exports = {
   Product,
   Category,
